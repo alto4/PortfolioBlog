01d254f14431446eb0bc8a6142bd0345c91086d2
Scott Alton <scottaltonmusic@gmail.com>
2021-04-20 13:08:16 -0400

    Sticky form added into new blog post page, redirects to successfully added post added.

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
+// Import Article model
+const Article = require("../models/article");
+
 // GET NEW BLOG POST ROUTE
 router.get("/new", (req, res) => {
-  res.render("articles/new");
+  res.render("articles/new", { article: new Article() });
+});
+
+// GET SINGLE BLOG POST ROUTE
+router.get("/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+
+  // If theres an error finding by id, redirect user to the homepage
+  if (article == null) {
+    res.redirect("/");
+  }
+
+  res.render("articles/show", { article: article });
 });
 
 // POST NEW BLOG POST ROUTE
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // Process new blog post
+  let article = new Article({
+    // Pass in options from form
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+
+  try {
+    // Redirect to newly posted post if succesful
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`);
+  } catch (err) {
+    // Refill form in case of invalid entry
+    res.render("articles/new", { article: article });
+  }
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 // Import article router
 const articleRouter = require("./routes/articles");
 
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // Use ejs view engine to convert/render views to HTML
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
 // GET HOME ROUTE - default
 app.get("/", (req, res, err) => {
@@ -36,5 +42,7 @@ app.get("/projects", (req, res, err) => {
 app.get("/contact", (req, res, err) => {
   res.render("contact");
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);

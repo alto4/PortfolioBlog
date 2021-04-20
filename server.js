e4cdb24cf96490fdea896e83c7adca1b412942d3
Scott Alton <scottaltonmusic@gmail.com>
2021-04-20 11:54:06 -0400

    Initial routes and folder structure setup.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+// Import article router
+const articleRouter = require("./routes/articles");
+
+// Use ejs view engine to convert/render views to HTML
+app.set("view engine", "ejs");
+
+app.use("/articles", articleRouter);
+
+// HOME ROUTE - default
+app.get("/", (req, res, err) => {
+  // Articles placeholder
+  const articles = [{}];
+
+  res.render("index", { articles: articles });
+});
+
+// ABOUT ROUTE
+app.get("/about", (req, res, err) => {
+  res.render("about");
+});
+
+// PROJECTS ROUTE
+app.get("/projects", (req, res, err) => {
+  res.render("projects");
+});
+
+// CONTACT ROUTE
+app.get("/contact", (req, res, err) => {
+  res.render("contact");
+});
+
+app.listen(5000);
